f73d69a41976926e4a9d954c54b90f8f0b8d88a9
Charles Goncalves <charles.fg@gmail.com>
2021-06-28 22:10:41 +0100

    adding the kvmqemu source

--- sync_page.sh
+++ sync_page.sh
@@ -1,3 +1,3 @@
-rsync -avz --exclude='.git/'  ~/git/page-dei/ charles@eden.dei.uc.pt:~/public_html
+rsync -avz --exclude='.git/'  ~/git/dei-site/ charles@eden.dei.uc.pt:~/public_html
 ssh charles@eden.dei.uc.pt 'chmod -R 755 ~/public_html'
 
