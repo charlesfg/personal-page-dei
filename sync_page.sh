ecb0ba1a5774d1251f4b69746eb54db5acb447c9
Charles Goncalves <charles.fg@gmail.com>
2023-12-28 09:19:51 +0000

    adding intrusion injection research

--- sync_page.sh
+++ sync_page.sh
@@ -1,3 +1,6 @@
-rsync -avz --exclude='.git/'  ~/git/dei-site/ charles@eden.dei.uc.pt:~/public_html
-ssh charles@eden.dei.uc.pt 'chmod -R 755 ~/public_html'
+#!/env/bash
+
+SITE_DIR=$(dirname $(realpath $0))
+rsync -avz --exclude='.git/'  ${SITE_DIR} charles@eden.dei.uc.pt:~/public_html
+ssh charles@eden.dei.uc.pt 'chmod -R 755 ~/public_html' 
 
