5ae01b26e22cc54aec45becc39217c48bd8f452b
Charles Goncalves <charles.fg@gmail.com>
2024-11-05 16:36:59 +0000

    sync the script

--- sync_page.sh
+++ sync_page.sh
@@ -1,6 +1,6 @@
 #!/env/bash
 
 SITE_DIR=$(dirname $(realpath $0))
-rsync -avz --exclude='.git/'  ${SITE_DIR} charles@eden.dei.uc.pt:~/public_html
+rsync -avz --exclude='.git/'  ${SITE_DIR}/ charles@eden.dei.uc.pt:~/public_html
 ssh charles@eden.dei.uc.pt 'chmod -R 755 ~/public_html' 
 
